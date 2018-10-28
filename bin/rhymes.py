e688c18394d6d730a631f26ab30689ddf10e6903
nejni-marji <nejni.marji@gmail.com>
2018-10-27 23:32:30 -0500

    Added try-except clause to protect against non-ascii input.

--- bin/rhymes.py
+++ bin/rhymes.py
@@ -6,7 +6,11 @@ from html import unescape
 
 def get_www_rhymes(query):
 	url = 'https://www.rhymezone.com/r/rhyme.cgi?Word=%s&typeofrhyme=perfect&org1=syl&org2=l&org3=y'
-	text = urlopen(url % query).read().decode('utf-8')
+	try:
+		text = urlopen(url % query).read().decode('utf-8')
+	except UnicodeEncodeError:
+		print('UnicodeEncodeError: returning []')
+		return []
 	pattern = '<a class=(?:"d r"|r) href="d=[^-].*?">(.*?)</a>'
 
 	matches = findall(pattern, text)
